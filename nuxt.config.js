ed56eff31a6a1c2c8108afcbce1dc28b5a504412
Akihiro Ikawa <akihiro.ikawa@fujifilm.com>
2020-10-26 18:01:38 +0900

    全ての静的ページを生成できるようconfigファイルを修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,3 +53,22 @@ export default {
     }
   }
 }
+import { projects } from "./utils/projectsData";
+
+  export default {
+    mode: "universal",
+
+    //...truncated
+
+    generate: {
+      async routes() {
+        const paths = [];
+
+        projects.forEach(project => {
+          paths.push(`/project/${project.slug}`);
+        });
+
+        return paths;
+      }
+    }
+  };
